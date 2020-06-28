adf7367b74bed7f75f3272686854a4ac663266ff
Erhan Yaşar <mail@erhanyasar.com.tr>
2020-06-28 14:26:34 +0300

    all components are ok

--- src/components/football-data/index.js
+++ src/components/football-data/index.js
@@ -8,15 +8,25 @@ export default class FootballMatchesData extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedYear: null
+      selectedYear: null,
+      totalMatches: 0,
+      resolvedArr: []
     };
   }
 
   onClick = (year) => (e) => {
-    // Code written in next line is to take care of adding active class to selected year for css purpose.
-    this.setState({
-      selectedYear: year
-    })
+    let url = "https://jsonmock.hackerrank.com/api/football_competitions?year=" + year;
+    fetch(url)
+      .then (response => response.json())
+      .then (result => {
+        let totalMatches = result.data.length;
+        
+        this.setState({ // Code written in next line is to take care of adding active class to selected year for css purpose.
+          selectedYear: year,
+          totalMatches: totalMatches,
+          resolvedArr: result.data
+        })
+      })
   }
 
   render() {
@@ -40,17 +50,19 @@ export default class FootballMatchesData extends Component {
             )
           })}
         </ul>
-
         <section className="content">
-          <section>
-            <div className="total-matches" data-testid="total-matches"></div>
-            
-            <ul className="mr-20 matches styled" data-testid="match-list">
-              <li className="slide-up-fade-in"> </li>
-            </ul>
-          </section>
-
-          <div data-testid="no-result" className="slide-up-fade-in no-result"></div>
+          {
+            this.state.totalMatches > 0 &&
+            <section>
+              <div className="total-matches" data-testid="total-matches">Total matches: { this.state.totalMatches }</div>
+              <ul className="mr-20 matches styled" id="match-list" data-testid="match-list">
+                {this.state.resolvedArr.map(item => (
+                  <li className="slide-up-fade-in" key={item}> Match { item.name } won by { item.winner } </li>
+                ))}
+              </ul>
+            </section>
+          }
+          <div data-testid="no-result" className="slide-up-fade-in no-result">No Matches Found</div>
         </section>
       </div>
     );
